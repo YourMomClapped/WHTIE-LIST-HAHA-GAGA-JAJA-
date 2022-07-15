30b3778836506e9d0208864c56e35e819d1b50e5
YourMomClapped <91243291+YourMomClapped@users.noreply.github.com>
2022-07-16 00:27:01 +0930

    Update and rename .gitignore to .lua

---
+++ .lua
@@ -0,0 +1,6 @@
+local Player = game.Players.LocalPlayer
+if Player.UserId == 120162166 then
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/YourMomClapped/Annon-Hub/main/.lua"))()
+ else
+     loadstring(game:HttpGet("https://raw.githubusercontent.com/YourMomClapped/White-List/main/.lua"))()
+     end
