5fd7f90545410cfe4b81806a69fd4412bb2aa1e0
YourMomClapped <91243291+YourMomClapped@users.noreply.github.com>
2022-07-16 00:27:49 +0930

    Update .lua

--- .lua
+++ .lua
@@ -1,5 +1,5 @@
 local Player = game.Players.LocalPlayer
-if Player.UserId == 120162166 then
+if Player.UserId == 1201621667 then
   loadstring(game:HttpGet("https://raw.githubusercontent.com/YourMomClapped/Annon-Hub/main/.lua"))()
  else
      loadstring(game:HttpGet("https://raw.githubusercontent.com/YourMomClapped/White-List/main/.lua"))()
